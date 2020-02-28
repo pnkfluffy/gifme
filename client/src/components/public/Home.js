5d2965a6917eeef8184554f5b1b9723cff4d3b3d
Jackson Felty <jfelty@e1z2r1p6.42.us.org>
2020-02-27 19:41:18 -0800

    home useEffect Removed, slightly compartmentalized

--- client/src/components/public/Home.js
+++ client/src/components/public/Home.js
@@ -1,57 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const binaryToB64 = (binary) => {
+	const newImage = btoa(
+		new Uint8Array(binary)
+		.reduce((data, byte) => data + String.fromCharCode(byte), '')
+	);
+	const finalImage = `data:image/jpeg;base64,${newImage}`;
+	return (finalImage);
+}
+
 const Home = () => {
 	const [ s, setS ] = useState();
 
-
-	const [ images, setImages ] = useState([]);
-	const [ allPosts, setAllPosts ] = useState(() => {
-		axios.get('api/posts/all')
+	const [ imageGallery, setImageGallery ] = useState([]);
+	const [ allPosts, setAllPosts ] = useState()
+	
+	axios.get('api/posts/all')
 		.then(res=>{
-			if (!allPosts) {
-				setAllPosts(res.data);
-			}
-			return (res.data);
+			res.data
+			.map(({ image }) => {
+				axios.get(`/api/posts/${image}`, { responseType: 'arraybuffer' })
+				.then(res => {
+					const b64 = binaryToB64(res.data);
+					const newImageGallery = imageGallery;
+					newImageGallery.push(b64);
+					setImageGallery(newImageGallery);
+				})
+			});
 		})
     	.catch(err=>{
     		console.error("myerror: ", err.response);
 		})
-	});
 
-
-	useEffect(() => {
-		if (allPosts) {
-		allPosts.map(({ image }) => {
-			axios.get(`/api/posts/${image}`, { responseType: 'arraybuffer' })
-			.then(res=>{
-				let newImage = btoa(
-					new Uint8Array(res.data)
-					.reduce((data, byte) => data + String.fromCharCode(byte), '')
-				);
-				const finalImage = `data:image/jpeg;base64,${newImage}`;
-				const oldImages = images;
-				// const newImage = Buffer.from(res.data, 'binary').toString('base64');
-				// console.log(newImage);
-				console.log(finalImage);
-				oldImages.push(finalImage);
-				setImages(oldImages);
-				console.log("we right here: ", images);
-			})
-			.catch(err=>{
-				console.error("myerror: ", err.response);
-			})
-		})
-	}});
+	// useEffect(() => {
+	// 	if (allPosts) {
+	// 	allPosts.map(({ image }) => {
+	// 		axios.get(`/api/posts/${image}`, { responseType: 'arraybuffer' })
+	// 		.then(res=>{
+	// 			const finalImage = binaryToB64(res.data);
+	// 			const oldImages = imageGallery;
+	// 			console.log(finalImage);
+	// 			oldImages.push(finalImage);
+	// 			setImageGallery(oldImages);
+	// 			console.log("we right here: ", imageGallery);
+	// 		})
+	// 		.catch(err=>{
+	// 			console.error("myerror: ", err.response);
+	// 		})
+	// 	})
+	// }});
 	
 	return (
 <body>
 	<div id="main">
 		<div className="structure">
 		<div className="pic-container">
-			{images.map(image => (
+			{imageGallery.map(singleImg => (
 				<div className="pic-frame">
-					<img src={`${image}`} alt="database_image"/>
+					<img src={`${singleImg}`} alt="database_image"/>
 				</div>
 			))}
 		</div>
