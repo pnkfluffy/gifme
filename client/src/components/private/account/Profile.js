05781b09620fd1fcf5b3265f99d994cdfaab5257
jsandler <jsandler.aol@gmail.com>
2020-03-18 11:23:24 -0700

    navbar and global profile frame

--- client/src/components/private/account/Profile.js
+++ client/src/components/private/account/Profile.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import ImageCard from "../../public/ImageCard";
+import ImageOverlay from "../../public/ImageOverlay";
 import { fetchAllPosts } from '../../../utils/FetchPosts';
+import fetchAuth from '../../../utils/FetchAuthclear';
 
 
-const MyPosts = () => {
+const Profile = () => {
     const [imageGallery, setImageGallery] = useState([]);
+	const [overlayData, setOverlayData] = useState(null);
+	const [authInfo, setAuthInfo] = useState(null);
 
     const authToken = localStorage.getItem('myToken');
 	const getPosts = async => {
@@ -23,25 +28,42 @@ const MyPosts = () => {
 	}
 
 	useEffect(() => {
+		setAuthInfo(fetchAuth());
 		getPosts();
-	}, [])
+	}, []);
+
+	var body = document.body;
+	const toggleOverlay = props => {
+	  body.classList.toggle('noscroll');
+	  if (props) {
+	    setOverlayData(props.imageData);
+	  } else {
+	    setOverlayData(null);
+	  }
+	};
     
     return (
-		<body>
-			<div id="main">
-				<div className="structure">
-					<div className="pic-container">
-						{imageGallery.map(singleImg => (
-							<div className="pic-frame">
-								<img src={`${singleImg.image}`} alt="database_image" />
-							</div>
-						))}
-					</div>
+		<div id="main">
+			<div className="structure">
+				<div className="pic-container">
+					{imageGallery.map(image => (
+        				<ImageCard
+        				  authInfo={authInfo}
+        				  imageData={image}
+        				  addOverlay={imageData => toggleOverlay({ imageData })}
+        				/>
+        				))}
 				</div>
 			</div>
-		</body>
+			{overlayData ? (
+                <ImageOverlay
+                  authInfo={authInfo}
+                  data={overlayData}
+                  removeOverlay={() => toggleOverlay(null)}
+                />
+              ) : null}
+		</div>
 	)
-
 }
 
-export default MyPosts;
+export default Profile;
